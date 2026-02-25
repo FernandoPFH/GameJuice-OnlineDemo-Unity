915749cbab1895518f160e2dc9580bf56a25c3fc
FernandoPFH <51247535+FernandoPFH@users.noreply.github.com>
2026-02-25 17:21:11 -0300

    Feat: Added Effect => Sound On Death Wall Hit

---
+++ GameJuice/Assets/Scripts/Effects/Sound/SoundOnDeathWallHit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundOnDeathWallHit_EffectSO", menuName = "EffectSO/SoundOnDeathWallHit")]
+public class SoundOnDeathWallHit : EffectSO
+{
+    [SerializeField] private AudioClip audio;
+
+    private void OnDeathWallHit(GameObject deathWall)
+    {
+        BallRefs.Instance.Audio.clip = audio;
+        BallRefs.Instance.Audio.pitch = 1f;
+        BallRefs.Instance.Audio.Play();
+    }
+
+    public override void OnEnabled()
+        => DeathWall.OnHit += OnDeathWallHit;
+
+    public override void OnDisabled()
+        => DeathWall.OnHit -= OnDeathWallHit;
+}
